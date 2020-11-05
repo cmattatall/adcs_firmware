4b983c13c0410442040fcb86aecb71236d732325
Carl Mattatall <cmattatall2@gmail.com>
2020-11-05 12:10:50 -0400

    add json parser to repo

--- Debug/subdir_vars.mk
+++ Debug/subdir_vars.mk
@@ -7,26 +7,32 @@ CMD_SRCS += \
 ../lnk_msp430f5529.cmd 
 
 C_SRCS += \
+../json.c \
 ../main.c \
 ../spi.c 
 
 C_DEPS += \
+./json.d \
 ./main.d \
 ./spi.d 
 
 OBJS += \
+./json.obj \
 ./main.obj \
 ./spi.obj 
 
 OBJS__QUOTED += \
+"json.obj" \
 "main.obj" \
 "spi.obj" 
 
 C_DEPS__QUOTED += \
+"json.d" \
 "main.d" \
 "spi.d" 
 
 C_SRCS__QUOTED += \
+"../json.c" \
 "../main.c" \
 "../spi.c" 
 
--- spi.c
+++ spi.c
@@ -1,8 +1,14 @@
-/*
- * spi.c
+/**
+ * @file spi.c
+ * @author Carl Mattatall (cmattatall2@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2020-11-05
  *
- *  Created on: Nov. 4, 2020
- *      Author: carl
+ * @copyright Copyright (c) 2020 LORRIS ADCS
+ *
+ * @note
+ * @todo
  */
 
 #include <msp430.h>
@@ -13,33 +19,34 @@
 #include "spi.h"
 
 
-static uint8_t *rx_outptr;
+static uint8_t *         rx_outptr;
 static volatile uint8_t *rx_inptr;
-static uint8_t rx_ringbuf[SPI_APPLICATION_BUFFER_SIZE];
-static volatile int rx_complete_signal = SPI_SIGNAL_CLR;
+static uint8_t           rx_ringbuf[SPI_APPLICATION_BUFFER_SIZE];
+static volatile int      rx_complete_signal = SPI_SIGNAL_CLR;
 
 
-static uint8_t tx_fifo[SPI_APPLICATION_BUFFER_SIZE];
+static uint8_t               tx_fifo[SPI_APPLICATION_BUFFER_SIZE];
 static volatile unsigned int tx_count;
-static unsigned int tx_bytes_loaded;
-static volatile int tx_complete_signal = SPI_SIGNAL_CLR;
+static unsigned int          tx_bytes_loaded;
+static volatile int          tx_complete_signal = SPI_SIGNAL_CLR;
 
 
-void SPI0_init(volatile int **receive_signal_watcher, volatile int **transmit_signal_watcher)
+void SPI0_init(volatile int **receive_signal_watcher,
+               volatile int **transmit_signal_watcher)
 {
     UCB0CTL1 |= UCSWRST; /* unlock ie: "reset" peripheral */
 
     /* Configure control registers */
-    UCB0CTL0 |= UCMST;      /* master */
-    UCB0CTL0 |= UCMODE_0;   /* mode 0 */
-    UCB0CTL0 |= UCMSB;      /* receive via msb first */
-    UCB0CTL0 |= UCSYNC;     /* Select SPI mode */
-    UCB0CTL0 |= UCCKPH;     /* falling edge clock phase (default) */
+    UCB0CTL0 |= UCMST;    /* master */
+    UCB0CTL0 |= UCMODE_0; /* mode 0 */
+    UCB0CTL0 |= UCMSB;    /* receive via msb first */
+    UCB0CTL0 |= UCSYNC;   /* Select SPI mode */
+    UCB0CTL0 |= UCCKPH;   /* falling edge clock phase (default) */
     UCB0CTL1 |= UCSSEL_2;
 
     /* Configure bitrate registers */
-    UCB0BR1   =  0x00;
-    UCB0BR0   =  0x01;
+    UCB0BR1 = 0x00;
+    UCB0BR0 = 0x01;
 
     /* Re-enable peripheral */
     UCB0CTL1 &= ~UCSWRST; /* Lock the peripheral control register config */
@@ -49,21 +56,21 @@ void SPI0_init(volatile int **receive_signal_watcher, volatile int **transmit_si
 
     /* Configure GPIO ports */
     P3SEL = BIT0 | BIT1 | BIT2; // Configures SPI
-    P2OUT &= ~BIT2;    //select device
+    P2OUT &= ~BIT2;             // select device
     P3OUT |= BIT1;
     P2OUT |= BIT2;
-    P2DIR |= BIT2;    //CONFIGURE CS FOR OUTPUT
+    P2DIR |= BIT2;        // CONFIGURE CS FOR OUTPUT
     P3DIR |= BIT0 | BIT2; // Set as outputs
     P3DIR &= ~BIT1;
 
-    P2OUT &= ~BIT2;//Make CS Low
+    P2OUT &= ~BIT2; // Make CS Low
 
 
     /* Configure receive interface */
-    rx_inptr = rx_ringbuf;
-    rx_outptr = rx_ringbuf;
+    rx_inptr           = rx_ringbuf;
+    rx_outptr          = rx_ringbuf;
     tx_complete_signal = SPI_SIGNAL_CLR;
-    if(receive_signal_watcher != NULL)
+    if (receive_signal_watcher != NULL)
     {
         *receive_signal_watcher = &rx_complete_signal;
     }
@@ -71,47 +78,40 @@ void SPI0_init(volatile int **receive_signal_watcher, volatile int **transmit_si
 
     /* Configure transmit interface */
     tx_complete_signal = SPI_SIGNAL_SET;
-    tx_count = 0;
-    tx_bytes_loaded = 0;
-    if(transmit_signal_watcher != NULL)
+    tx_count           = 0;
+    tx_bytes_loaded    = 0;
+    if (transmit_signal_watcher != NULL)
     {
         *transmit_signal_watcher = &tx_complete_signal;
     }
-
 }
 
 
 int SPI0_receive_payload(uint8_t *userbuf, uint16_t len)
 {
-    /** @todo */
-}
-
-
-int SPI0_receive_payload(uint8_t *userbuf, uint16_t len)
-{
-    unsigned int i = 0;
+    unsigned int i           = 0;
     unsigned int delim_found = 0;
     do
     {
         userbuf[i] = *rx_outptr;
         rx_outptr++;
-        if(rx_outptr == &rx_ringbuf[sizeof(rx_ringbuf) - 1])
+        if (rx_outptr == &rx_ringbuf[sizeof(rx_ringbuf) - 1])
         {
             rx_outptr = rx_ringbuf;
         }
 
-        if(userbuf[i] == SPI_DELIM_CHAR)
+        if (userbuf[i] == SPI_DELIM_CHAR)
         {
             delim_found = 1;
-            if(rx_complete_signal == SPI_SIGNAL_SET)
+            if (rx_complete_signal == SPI_SIGNAL_SET)
             {
                 rx_complete_signal = SPI_SIGNAL_CLR;
             }
         }
 
-    } while(++i < len && !delim_found);
+    } while (++i < len && !delim_found);
 
-    if(delim_found)
+    if (delim_found)
     {
         return 0;
     }
@@ -122,10 +122,10 @@ int SPI0_receive_payload(uint8_t *userbuf, uint16_t len)
 }
 
 
-unsigned int SPI0_transmit_IT(uint8_t* bytes, uint16_t len)
+unsigned int SPI0_transmit_IT(uint8_t *bytes, uint16_t len)
 {
     /* Don't clobber ongoing transmission */
-    if(tx_complete_signal == SPI_SIGNAL_CLR)
+    if (tx_complete_signal == SPI_SIGNAL_CLR)
     {
         return 0; /* failure */
     }
@@ -137,13 +137,13 @@ unsigned int SPI0_transmit_IT(uint8_t* bytes, uint16_t len)
         unsigned int copy_len = len;
 
         /* Only load as much as we can fit */
-        if(len > sizeof(tx_fifo))
+        if (len > sizeof(tx_fifo))
         {
             copy_len = sizeof(tx_fifo);
         }
 
         memcpy(tx_fifo, bytes, copy_len); /** @todo TRANSFER VIA DMA */
-        tx_count = 0;
+        tx_count        = 0;
         tx_bytes_loaded = copy_len;
 
         /* Load first byte and let ISR do the rest */
@@ -154,39 +154,38 @@ unsigned int SPI0_transmit_IT(uint8_t* bytes, uint16_t len)
 }
 
 
-__attribute__((interrupt(USCI_B0_VECTOR)))
-void USCI_B0_ISR (void)
+__attribute__((interrupt(USCI_B0_VECTOR))) void USCI_B0_ISR(void)
 {
     /* If receive interrupt is pending*/
-    if(UCB0IV & 0X02)
+    if (UCB0IV & 0X02)
     {
         *rx_inptr = UCB0RXBUF;
 
         /* If we've received the delimiter, signal application */
-        if(*rx_inptr == SPI_DELIM_CHAR)
+        if (*rx_inptr == SPI_DELIM_CHAR)
         {
             rx_complete_signal = SPI_SIGNAL_SET;
         }
 
         /* advance in-pointer for next incoming byte */
         rx_inptr++;
-        if(rx_inptr == &rx_ringbuf[sizeof(rx_ringbuf) - 1])
+        if (rx_inptr == &rx_ringbuf[sizeof(rx_ringbuf) - 1])
         {
             rx_inptr = rx_ringbuf;
         }
     }
 
     /* TX interrupt. indicates when TXBUF is can be written */
-    else if(UCB0IV & 0X04)
+    else if (UCB0IV & 0X04)
     {
-        status = UCB0STAT; /* check if not busy */
-        if(!(UCB0STAT & UCBUSY))
+        if (!(UCB0STAT & UCBUSY))
         {
-            if(tx_count < tx_bytes_loaded)
+            if (tx_count < tx_bytes_loaded)
             {
-                if(UCB0STAT & UCOE)
+                if (UCB0STAT & UCOE)
                 {
-                    /* Transmit the missed byte if previous transmission overran */
+                    /* Transmit the missed byte if previous transmission overran
+                     */
                     UCB0TXBUF = tx_fifo[tx_count];
                 }
                 else
@@ -198,11 +197,10 @@ void USCI_B0_ISR (void)
             else
             {
                 tx_complete_signal = SPI_SIGNAL_SET;
-                tx_bytes_loaded = 0;
-                tx_count = 0;
+                tx_bytes_loaded    = 0;
+                tx_count           = 0;
                 UCB0IE &= ~UCTXIE;
             }
         }
     }
 }
-
