6062e327bfc3c7f2499886bdca9b2f1e374d2f0a
Carl Mattatall <cmattatall2@gmail.com>
2020-12-16 16:03:55 -0400

    fix linux thing

--- compile.py
+++ compile.py
@@ -1,9 +1,10 @@
 import os
 import sys
-import pathlib
 import subprocess
 import argparse
 import platform
+import pathlib
+
 
 def checkPythonVersion():
     if sys.version_info.major < 3: # python 3 must be the runtime
@@ -13,15 +14,18 @@ if __name__ == "__main__":
     checkPythonVersion() 
 
     # user changes this for now
-    crossCompiling = "ON"
+    crossCompiling = "OFF"
 
     if(platform.system() == "Windows"):
         os.system("cmake -S . -B build -G \"MinGW Makefiles\" -DCMAKE_TOOLCHAIN_FILE=\"D:\\Desktop\\MyDev\\adcs_firmware\\toolchain.cmake\" -DCMAKE_CROSSCOMPILING=\"%s\"" % (crossCompiling))
         os.system("cmake --build build")
 
     elif(platform.system() == "Linux"):
-        os.system("cmake -S . -B build -G \"Unix Makefiles\" -DCMAKE_TOOLCHAIN_FILE=\"D:\\Desktop\\MyDev\\adcs_firmware\\toolchain.cmake\" -DCMAKE_CROSSCOMPILING=\"%s\"" % (crossCompiling))
+        os.system("rm -r build")
+        os.system("cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=\"toolchain.cmake\" -DCMAKE_CROSSCOMPILING=\"%s\"" % (crossCompiling))
     else:
         printf("%s not supported" % (platform.system()))
         exit(1)
+    
+    os.system("cmake --build build")
     
