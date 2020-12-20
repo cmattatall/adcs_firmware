e2393fc1815f042300b6936a2bfc22903762cb13
Carl Mattatall <cmattatall2@gmail.com>
2020-12-20 11:57:40 -0400

    sync jtok

--- bufferlib/build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/carl/workspace_v10/adcs_firmware/bufferlib/test
-# Build directory: /home/carl/workspace_v10/adcs_firmware/bufferlib/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
