a8642d730afa7fbf3035097bf61b8a6efaa640c4
Carl Mattatall <cmattatall2@gmail.com>
2020-12-27 12:00:08 -0400

    add javadoc headers to reaction wheel tests. Pass all regression tests

--- core/reaction_wheels/test/reactionwheel_y_dir_set_check.c
+++ core/reaction_wheels/test/reactionwheel_y_dir_set_check.c
@@ -1,3 +1,20 @@
+/**
+ * @file reactionwheel_y_dir_set_check.c
+ * @author Carl Mattatall (cmattatall2@gmail.com)
+ * @brief Source module to test reactionwheel interface API functionality for
+ * setting/getting reaction wheel Y direction value
+ * @version 0.1
+ * @date 2020-12-27
+ * 
+ * @copyright Copyright (c) 2020 Carl Mattatall
+ * 
+ * @note
+ */
+#if defined(TARGET_MCU)
+#error NATIVE TESTS CANNOT BE RUN ON A BARE METAL MICROCONTROLLER
+#endif /* #if defined(TARGET_MCU) */
+
+#include <stdint.h>
 #include <stdlib.h>
 #include <limits.h>
 
@@ -7,7 +24,7 @@ int main(void)
 {
     /* Expect set direction is stored correclty */
     RW_DIR_t dirs[2] = {RW_DIR_anticlockwise, RW_DIR_clockwise};
-    int      i;
+    uint16_t      i;
     for (i = 0; i < 2; i++)
     {
         reacwheel_set_wheel_dir(REACTION_WHEEL_y, dirs[i]);
@@ -20,7 +37,7 @@ int main(void)
     /* Expect full range of values that are not a given direction to
      * return INVALID
      */
-    for (i = RW_DIR_invalid; i < INT_MAX; i++)
+    for (i = RW_DIR_invalid; i < UINT16_MAX; i++)
     {
         if (RW_DIR_invalid != reacwheel_set_wheel_dir(REACTION_WHEEL_y, i))
         {
