6a6fe870c57d52fcc36590637a2095cd8cb1224a
Carl Mattatall <cmattatall2@gmail.com>
2020-12-29 10:44:14 -0400

    Add dockerfile to test toolchain installation script

--- resources/examples/uart_example.c
+++ resources/examples/uart_example.c
@@ -1,41 +1,25 @@
 /**
- * @file uart.c
+ * @file uart_example.c
  * @author Carl Mattatall (cmattatall2@gmail.com)
- * @brief UART peripheral file
+ * @brief Source module to demonstrate use of the UART through USCI_A0
  * @version 0.1
- * @date 2020-12-09
+ * @date 2020-12-29
  *
- * @copyright Copyright (c) 2020
+ * @copyright Copyright (c) 2020 Carl Mattatall
  *
  * @note
- * @todo
  */
-#include <stdbool.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h> /* memcpy */
-
-#include "attributes.h"
-
-#include "targets.h"
-
-#if defined(TARGET_MCU)
 #include <msp430.h>
 
-#include "uart.h"
+static int uart_transmit(uint8_t *buf, uint_least16_t buflen);
 
 static volatile bool tx_cplt = false;
-static void (*uart_receive_byte)(uint8_t);
 
 
-/**
- * @note PLEASE READ https://www.ti.com/lit/ug/slau144j/slau144j.pdf
- * PAGE 424 before touching this function
- */
-
-void uart_init(receive_func rx)
+int main(void)
 {
-    P3SEL = BIT3 + BIT4;                    // P3.4,5 = USCI_A0 TXD/RXD
+    WDTCTL = WDTPW + WDTHOLD;               // Stop WDT
+    P3SEL  = BIT3 + BIT4;                   // P3.4,5 = USCI_A0 TXD/RXD
     UCA0CTL1 |= UCSWRST;                    // **Put state machine in reset**
     UCA0CTL1 |= UCSSEL_2;                   // SMCLK
     UCA0BR0  = 6;                           // 1MHz 9600 (see User's Guide)
@@ -49,27 +33,9 @@ void uart_init(receive_func rx)
     uart_receive_byte = rx;
 }
 
-void uart_deinit(void)
-{
-    /** @todo
-     *  - RESET REGISTERS TO DEFAULT VAULES
-     *  - FLUSH RX AND TF FIFO
-     *  - CLEAR INTERRUPT FLAGS
-     *  - DISABLE INTERRUPTS
-     *  - RESET CONFIGURATION OF USCI_A0 to defaults
-     *  - DISABLE CLOCK MUX to peripheral (optional - improves power savings)
-     */
-
-    if (uart_receive_byte != NULL)
-    {
-        uart_receive_byte = NULL;
-    }
-
-    log_trace("deinitialized uart\n");
-}
 
 
-int uart_transmit(uint8_t *buf, uint_least16_t buflen)
+static int uart_transmit(uint8_t *buf, uint_least16_t buflen)
 {
     CONFIG_ASSERT(buf != NULL);
     tx_cplt         = true;
@@ -85,7 +51,6 @@ int uart_transmit(uint8_t *buf, uint_least16_t buflen)
     } while (i <= buflen);
     return i;
 }
-
 
 __attribute__((used, interrupt(USCI_A0_VECTOR))) void USCI_A0_VECTOR_ISR(void)
 {
@@ -113,5 +78,3 @@ __attribute__((used, interrupt(USCI_A0_VECTOR))) void USCI_A0_VECTOR_ISR(void)
         break;
     }
 }
-
-#endif /* #if defined(TARGET_MCU) */
