ec81c65f0f4345be1640fb77aa55252c99d60206
Carl Mattatall <cmattatall2@gmail.com>
2021-01-01 17:31:30 -0400

    add watchdog timer functionality to system

--- core/main.c
+++ core/main.c
@@ -23,7 +23,13 @@ static uint8_t msg[128];
 int main(void)
 {
 #if defined(TARGET_MCU)
+
+#if defined(DEBUG)
     watchdog_stop();
+#else
+    watchdog_start();
+#endif /* #if defined(DEBUG) */
+
 
     OBC_IF_config(OBC_IF_PHY_CFG_UART);
 
@@ -54,5 +60,10 @@ int main(void)
             }
             OBC_IF_dataRxFlag_write(OBC_IF_DATA_RX_FLAG_CLR);
         }
+
+
+#if !defined(DEBUG)
+        watchdog_kick();
+#endif /* #if !defined(DEBUG) */
     }
 }
