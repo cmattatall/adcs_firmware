509ba9e1d116d10f3ac0455f33367ef550e715e4
Carl Mattatall <cmattatall2@gmail.com>
2021-01-02 15:06:53 -0400

    add template code for inertial measurement unit

---
+++ core/inertial_measurement_unit/inc/imu.h
@@ -0,0 +1,17 @@
+#ifndef __IMU_H__
+#define __IMU_H__
+#ifdef __cplusplus
+/* clang-format off */
+extern "C"
+{
+/* clang-format on */
+#endif /* Start C linkage */
+
+
+
+#ifdef __cplusplus
+/* clang-format off */
+}
+/* clang-format on */
+#endif /* End C linkage */
+#endif /* __IMU_H__ */
---
+++ core/inertial_measurement_unit/src/imu.c
@@ -0,0 +1,21 @@
+/**
+ * @file imu.c
+ * @author Carl Mattatall (cmattatall2@gmail.com)
+ * @brief Source module for the inertial measurement unit API
+ * @version 0.1
+ * @date 2021-01-02
+ * 
+ * @copyright Copyright (c) 2021 Carl Mattatall
+ * 
+ */
+
+#include "imu.h"
+
+#if defined(TARGET_MCU)
+
+/** @todo I DON'T KNOW WHAT PHY INTERFACE IS BEING USED TO TALK TO THE
+ * ACCELEROMETER */
+
+#else
+
+#endif /* #if defined(TARGET_MCU) */
