182fa320a28fc04b4efa8411b51a1cd689318dbc
Carl Mattatall <cmattatall2@gmail.com>
2021-01-08 12:00:17 -0400

    Example: add initial template for sun sensor driver example

---
+++ drivers/examples/sun_sensor.c
@@ -0,0 +1,44 @@
+/**
+ * @file sun_sensor.c
+ * @author Carl Mattatall (cmattatall2@gmail.com)
+ * @brief Example executable to demonstrate reading from the sun sensor
+ * @version 0.1
+ * @date 2021-01-08
+ *
+ * @copyright Copyright (c) 2021 Carl Mattatall
+ *
+ *
+ * @note
+ * PINOUT:
+ ***************************************
+ *   MSP430                   SUN_SENSOR
+ *   5V --------------------------- 5V
+ *
+ *   GND -------------------------- GND
+ *
+ *   P3.0 (UCB0 MOSI) ------------- MOSI
+ *
+ *   P3.1 (UCB0 MISO) ------------- MISO
+ *
+ *   P3.2 (UCB0CLK) --------------- CLK
+ *
+ *   NC                             CS
+ *
+ *   NC                             3V3
+ ****************************************
+ */
+
+
+int main(void)
+{
+
+
+
+    while(1)
+    {
+        
+    }
+}
+
+
+
