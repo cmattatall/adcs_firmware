ab36506e228958eca8fd588de4ae5efc5ec2fc19
Carl Mattatall <cmattatall2@gmail.com>
2021-01-16 16:58:03 -0400

    Add initial work for sun sensor API

--- core/sun_sensors/src/sun_sensors.c
+++ core/sun_sensors/src/sun_sensors.c
@@ -30,6 +30,7 @@ static SUNSEN_intensity_t SUNSEN_get_sun_vector(void);
 
 SUNSEN_measurement_t SUNSEN_measure_intensities(void)
 {
+#if defined(TARGET_MCU)
     SUNSEN_measurement_t measurement;
     memset((void *)&measurement, 0, sizeof(measurement));
     unsigned int i;
@@ -38,19 +39,28 @@ SUNSEN_measurement_t SUNSEN_measure_intensities(void)
         measurement.vals[i] = SUNSEN_get_sun_vector();
     }
     return measurement;
+#else
+
+
+#endif /* #if defined(TARGET_MCU) */
 }
 
 
 static SUNSEN_intensity_t SUNSEN_get_sun_vector(void)
 {
+#if defined(TARGET_MCU)
     ADS7841_driver_init(SUNSEN_enable_ADS7841, SUNSEN_disable_ADS7841,
                         ADS7841_PWRMODE_always_on, ADS7841_CONVMODE_12);
-    SUNSEN_intensity_t bearing;
-    bearing.first  = ADS7841_measure_channel(ADS7841_CHANNEL_1);
-    bearing.second = ADS7841_measure_channel(ADS7841_CHANNEL_2);
-    bearing.third  = ADS7841_measure_channel(ADS7841_CHANNEL_3);
+    SUNSEN_intensity_t measurement;
+    measurement.first  = ADS7841_measure_channel(ADS7841_CHANNEL_1);
+    measurement.second = ADS7841_measure_channel(ADS7841_CHANNEL_2);
+    measurement.third  = ADS7841_measure_channel(ADS7841_CHANNEL_3);
     ADS7841_driver_deinit();
-    return bearing;
+    return measurement;
+#else
+
+
+#endif /* #if defined(TARGET_MCU) */
 }
 
 
