063f6fc7ec7f2c218cdc9cbc51426dad091b2f57
Carl Mattatall <cmattatall2@gmail.com>
2021-01-19 10:39:38 -0400

    Add automation of static analysis to project

--- share/attributes.h
+++ share/attributes.h
@@ -9,18 +9,14 @@ extern "C"
 
 /* If you like preprocessor abuse, you're gonna love this file... */
 
-#define DO_PRAGMA(x) _Pragma(#x)
-
 /* clang-format off */
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
 #ifdef __interrupt
 #undef __interrupt
 #endif
 #define __interrupt(isr) _Pragma vector = isr __interrupt
-#elif defined(__GNUC__)
+#elif defined(__GNUC__) || defined(__GNUG__)
 #define __weak __attribute__((weak))
-#else
-#error Compiler not supported!
 #endif
 
 #ifndef __EMULATABLE
