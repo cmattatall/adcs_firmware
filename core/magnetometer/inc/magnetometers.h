e5df300a1aa25022a89711ad73d9a91503dee81b
Carl Mattatall <cmattatall2@gmail.com>
2021-02-22 14:26:47 -0400

    add sub section for magnetometers

---
+++ core/magnetometer/inc/magnetometers.h
@@ -0,0 +1,17 @@
+#ifndef __MAGNETOMETERS_H__
+#define __MAGNETOMETERS_H__
+#ifdef __cplusplus
+/* clang-format off */
+extern "C"
+{
+/* clang-format on */
+#endif /* Start C linkage */
+
+
+
+#ifdef __cplusplus
+/* clang-format off */
+}
+/* clang-format on */
+#endif /* End C linkage */
+#endif /* __MAGNETOMETERS_H__ */
---
+++ core/magnetometer/src/magnetometers.c
@@ -0,0 +1,82 @@
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "attributes.h"
+#include "config_assert.h"
+#include "sun_sensors.h"
+
+#if defined(TARGET_MCU)
+#include "spi.h"
+#include "ads7841e.h"
+#else
+#endif /* #if defined(TARGET_MCU) */
+
+
+
+#if 0 
+static void               SUNSEN_enable_ADS7841(void);
+static void               SUNSEN_disable_ADS7841(void);
+static SUNSEN_intensity_t SUNSEN_get_sun_vector(void);
+
+
+SUNSEN_measurement_t SUNSEN_measure_intensities(void)
+{
+#if defined(TARGET_MCU)
+    SUNSEN_measurement_t measurement;
+    memset((void *)&measurement, 0, sizeof(measurement));
+    unsigned int i;
+    for (i = 0; i < NUM_SUN_SENSORS; i++)
+    {
+        measurement.vals[i] = SUNSEN_get_sun_vector();
+    }
+    return measurement;
+#else
+
+
+#endif /* #if defined(TARGET_MCU) */
+}
+
+
+static SUNSEN_intensity_t SUNSEN_get_sun_vector(void)
+{
+#if defined(TARGET_MCU)
+    ADS7841_driver_init(SUNSEN_enable_ADS7841, SUNSEN_disable_ADS7841,
+                        ADS7841_PWRMODE_stayOn, ADS7841_BITRES_12);
+    SUNSEN_intensity_t measurement;
+    measurement.first  = ADS7841_measure_channel(ADS7841_CHANNEL_SGL_1);
+    measurement.second = ADS7841_measure_channel(ADS7841_CHANNEL_SGL_2);
+    measurement.third  = ADS7841_measure_channel(ADS7841_CHANNEL_SGL_3);
+    ADS7841_driver_deinit();
+    return measurement;
+#else
+
+
+#endif /* #if defined(TARGET_MCU) */
+}
+
+
+static void SUNSEN_enable_ADS7841(void)
+{
+#if defined(TARGET_MCU)
+
+    /** @todo SET SPI CS MUX TO DRIVE CS PIN OF ADS7841 ON SUN SENSOR LOW */
+
+#warning NOT IMPLEMENTED YET
+#else
+
+
+#endif /* #if defined(TARGET_MCU) */
+}
+
+
+static void SUNSEN_disable_ADS7841(void)
+{
+#if defined(TARGET_MCU)
+#warning NOT IMPLEMENTED YET
+    /** @todo SET SPI CS MUX TO DRIVE CS PIN OF ADS7841 ON SUN SENSOR HIGH */
+#else
+#endif /* #if defined(TARGET_MCU) */
+}
+
+#endif
