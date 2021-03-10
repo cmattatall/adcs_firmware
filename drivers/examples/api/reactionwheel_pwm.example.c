3764169dd70a269196d5e8d5b76a9efe9d11eea1
Carl Mattatall <cmattatall2@gmail.com>
2021-03-10 11:45:05 -0400

    mostly fix pwm example for reactionwheels

--- drivers/examples/api/reactionwheel_pwm.example.c
+++ drivers/examples/api/reactionwheel_pwm.example.c
@@ -25,8 +25,11 @@
 #define API_TIMER_MC_INIT_MODE (MC__CONTINUOUS)
 #define API_TIMER_TASSEL_INIT_MODE (TASSEL__SMCLK)
 
-static void RW_PWM_gpio_init(void)
+
+int main()
 {
+    WDTCTL = WDTPW + WDTHOLD;
+
     /* PIN 2.3 (RW_X_SPEED_CTRL) */
     P2DIR |= BIT3;
     P2SEL |= BIT3;
@@ -38,47 +41,40 @@ static void RW_PWM_gpio_init(void)
     /* PIN 2.5 (RW_Z_SPEED_CTRL) */
     P2DIR |= BIT5;
     P2SEL |= BIT5;
-}
 
-
-static void RW_PWM_timer_init(void)
-{
-    TA2CTL &= ~(MC0 | MC1); /* Stop timer A0 */
-
-    /* Set Timer A0 clock source to smclk */
-    TA2CTL = (TA2CTL & ~(TASSEL0 | TASSEL1)) | API_TIMER_TASSEL_INIT_MODE;
+    TA2CTL &= ~(MC0 | MC1); /* Stop timer A2 */
 
     /* Confgigure pwm on pin 2.3 (TA.2.0) */
-    TA2CCR0  = 0;
-    TA2CCTL0 = (TA2CCTL0 & ~(OUTMOD0 | OUTMOD1 | OUTMOD2)) |
-               API_TIMER_OUTMOD_INIT_MODE;
+    TA2CCR0 = 0;
+    TA2CCTL0 &= ~(OUTMOD0 | OUTMOD1 | OUTMOD2);
+    TA2CCTL0 |= OUTMOD_TOG_SET;
 
     /* Confgigure pwm on pin 2.4 (TA.2.1) */
-    TA2CCR1  = 0;
-    TA2CCTL1 = (TA2CCTL1 & ~(OUTMOD0 | OUTMOD1 | OUTMOD2)) |
-               API_TIMER_OUTMOD_INIT_MODE;
+    TA2CCR1 = 0;
+    TA2CCTL1 &= ~(OUTMOD0 | OUTMOD1 | OUTMOD2);
+    TA2CCTL1 |= OUTMOD_TOG_SET;
 
     /* Confgigure pwm on pin 2.5 (TA.2.2) */
-    TA2CCR2  = 0;
-    TA2CCTL2 = (TA2CCTL2 & ~(OUTMOD0 | OUTMOD1 | OUTMOD2)) |
-               API_TIMER_OUTMOD_INIT_MODE;
-
-    /* Set timer A2 count mode */
-    TA2CTL = (TA2CTL & ~(MC0 | MC1)) | API_TIMER_MC_INIT_MODE;
-}
+    TA2CCR2 = 0;
+    TA2CCTL2 &= ~(OUTMOD0 | OUTMOD1 | OUTMOD2);
+    TA2CCTL2 |= OUTMOD_TOG_SET;
 
 
-int main()
-{
-    WDTCTL = WDTPW + WDTHOLD;
-    RW_PWM_gpio_init();
-    RW_PWM_timer_init();
+    /* Set Timer A2 clock source to smclk */
+    TA2CTL &= ~(TASSEL0 | TASSEL1);
+    TA2CTL |= TASSEL__SMCLK;
 
-    TA2CCR0 = 20000;
+    /* Set count mode */
+    TA2CTL &= ~(MC0 | MC1);
+    TA2CTL |= MC__CONTINOUS;
+
+    /* so for some reason, uncommenting this line absolultely */
+    /* causes timerA2 to stop working */
+    // TA2CCR0 = 20000;
     TA2CCR1 = 20000;
     TA2CCR2 = 20000;
 
-    _BIS_SR(GIE);
+    _BIS_SR(GIE + LPM0_bits);
 
     while (1)
     {
