3d2dddb03c93816e3b3d3d7ef02d6cbf9717b3f7
Carl Mattatall <cmattatall2@gmail.com>
2021-03-10 11:30:14 -0400

    put timerA2 pin 2.3 pwm example on hold (suspect silicon errata)

--- drivers/examples/peripherals/timerA2_pwm_pin_2_3.example.c
+++ drivers/examples/peripherals/timerA2_pwm_pin_2_3.example.c
@@ -37,9 +37,9 @@ int main(void)
     TA2CTL &= ~(MC0 | MC1);
     TA2CTL |= MC__CONTINUOUS;
 
-    _BIS_SR(GIE + LPM0_bits);
-
     TA2CCR0 = 20000; /* Initialize compare register value to 0 */
+
+    _BIS_SR(GIE + LPM0_bits);
 
     while (1)
     {
